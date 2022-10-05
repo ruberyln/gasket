123e3e305577fab15b9ee90fa1a6725cdcb8ca4e
Jesutofunmi <tofunalabi@gmail.com>
2022-10-04 19:58:36 -0600

    3 color gasket

--- gasket.js
+++ gasket.js
@@ -1,6 +1,6 @@
 "use strict";
 var gl;
-
+var colors = [];
 var positions=[];
 
 var numPositions=1000000;
@@ -24,14 +24,21 @@ vec2(-1, -1),
 vec2(0, 1),
 vec2(1,-1)
 ];
+var vcolor = [
+    vec4(0.5,0.25,0.92,1.0),
+    vec4(0.5,0.64,0.5,1.0),
+    vec4(0.99,0.0,0.4,1.0),
+   
+]
+var p = vertices[0]
 
-var u = add(vertices[0], vertices[1]);
-var v = add(vertices[0], vertices[2]);
-var p = mult(0.25, add( u, v ));
+//var u = add(vertices[0], vertices[1]);
+//var v = add(vertices[0], vertices[2]);
+//var p = mult(0.25, add( u, v ));
 
 
 positions.push(p);
-
+colors.push(vcolor[0]);
 
 
 for (var i = 0; positions.length < numPositions; ++i){
@@ -40,6 +47,8 @@ for (var i = 0; positions.length < numPositions; ++i){
     p = add(positions[i], vertices[j]);
     p = mult(0.5,p);
     positions.push(p);
+    colors.push(vcolor[j]);
+
 }
 
 //viewport consist of where the gasket will be viewed (x,y,w,h)
@@ -48,17 +57,18 @@ gl.viewport(0, 0,canvas.width, canvas.height);
 // color of the background canvas  using RGBA
 
 // this is white with full opacity
-gl.clearColor(1.0,0.0,0.5,1.0);
+gl.clearColor(1.0, 1.0, 1.0, 1.0);
 
 //load the shaders from Init shaders into a program object 
 var program = initShaders( gl, "vertex-shader", "fragment-shader");
 gl.useProgram (program);
 //create buffer 
-var bufferId = gl.createBuffer();
+var buffer = gl.createBuffer();
 //bind buffer
-gl.bindBuffer (gl.ARRAY_BUFFER, bufferId);
+gl.bindBuffer (gl.ARRAY_BUFFER, buffer);
 //load data in buffer
 gl.bufferData (gl.ARRAY_BUFFER, flatten(positions), gl.STATIC_DRAW);
+
 
 //get attribute location of vertex shader
 var positionLoc = gl.getAttribLocation(program, "vPosition");
@@ -66,6 +76,19 @@ var positionLoc = gl.getAttribLocation(program, "vPosition");
     //enable vertex shader
     gl.enableVertexAttribArray(positionLoc);
 
+//....
+    //create color buffer 
+var cBuffer= gl.createBuffer();
+//bind buffer
+gl.bindBuffer (gl.ARRAY_BUFFER, cBuffer);
+//load data in buffer
+gl.bufferData (gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
+
+
+var colorLoc = gl.getAttribLocation(program, 'aColor');
+    gl.vertexAttribPointer(colorLoc, 4, gl.FLOAT, false, 0, 0);
+    //enable vertex shader
+    gl.enableVertexAttribArray(colorLoc);
 render();
  };
 
